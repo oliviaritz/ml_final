ecc4975ae2ca612544b1a2fd5107a6a25af7ebe2
rkatya19 <rkatya19@vt.edu>
2019-04-19 14:09:56 -0400

    data still too big :(

--- subset_data.py
+++ subset_data.py
@@ -7,13 +7,19 @@ d = enchant.Dict("en_US")
 
 with open("./embeddings/glove.6B.50d.txt", "r", encoding='utf8') as f:
     lines = f.readlines()
-	
+
+count = 0
 with open("./embeddings/glove.6B.50d_small.txt", "w", encoding='utf8') as f:
     for line in lines:
+        if count == 26000:
+            break;
+
         word = line.split()[0]
         contains_num = bool(re.search(r'\d', word))
         if d.check(word):
-		    if word == "he" or word == "she":
+            if word == "he" or word == "she":
                 f.write(line)
-            else if not contains_num and not in ENGLISH_STOP_WORDS:
+                count += 1
+            elif (not contains_num) and (word not in ENGLISH_STOP_WORDS):
                 f.write(line)
+                count += 1
