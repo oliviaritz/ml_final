2e35d407854a53572dfe29d64bb6314974bddefc
Olivia Ritz <oliviar@vt.edu>
2019-04-21 16:01:45 -0400

    everything working

--- experiment.py
+++ experiment.py
@@ -4,16 +4,11 @@ from matplotlib import pyplot as plt
 import json
 import random
 import numpy as np
-
-import debiaswe as dwe
-import debiaswe.we as we
-from debiaswe.we import WordEmbedding
-from debiaswe.data import load_professions
-from debiaswe.debias import debias
+from we import WordEmbedding
+from debias import debias
 
 
-
-E = WordEmbedding('./embeddings/glove.6B.50d_small.txt')
+E = WordEmbedding('./embeddings/glove.6B.300d_small.txt')
 
 # Lets load some gender related word lists to help us with debiasing
 with open('./data/definitional_pairs.json', "r") as f:
@@ -30,19 +25,49 @@ v_gender = E.diff('she', 'he')
 
 
 # gender analogies before debiasing
-a_gender = E.best_analogies_dist_thresh(v_gender)
+a_gender = E.best_analogies_dist_thresh(v_gender, 1, 150, 50000)
 
+'''
 print("ANALOGIES BEFORE")
 for (a,b,c) in a_gender:
     print(a+"-"+b)
-	
+'''
+
 
 debias(E, gender_specific_words, defs, equalize_pairs)
 
 
 # gender analogies after debiasing
-a_gender_db = E.best_analogies_dist_thresh(v_gender)
+a_gender_db = E.best_analogies_dist_thresh(v_gender, 1, 150, 50000)
 
+'''
 print("ANALOGIES AFTER")
 for (a,b,c) in a_gender_db:
     print(a+"-"+b)
+'''
+
+# mix before and after biasing analogies and write them out to file for voting
+
+#add labels
+a_gender = [list(tup)+["b"] for tup in a_gender]
+a_gender_db = [list(tup)+["db"] for tup in a_gender_db]
+
+#randomize biased and debiased lists together
+mixed_list = a_gender + a_gender_db
+random.shuffle(mixed_list)
+
+wout_labels = open('non_labeled.txt','a')
+wout_labels.truncate(0)
+
+w_labels = open('labeled.txt','a')
+w_labels.truncate(0)
+
+for (a,b,c,d) in mixed_list:
+    out1 = a + "-" + b +"\n"
+    wout_labels.write(out1)
+
+    out2 = a + "-" + b + " " + d + "\n"
+    w_labels.write(out2)
+
+wout_labels.close()
+w_labels.close()
--- subset_data.py
+++ subset_data.py
@@ -1,26 +1,24 @@
 #import enchant
 import re
+import json
 
-from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
-from nltk.corpus import wordnet
+# cite : https://github.com/dwyl/english-words
+with open('./data/words_dictionary.json') as json_file:
+    dictionary = json.load(json_file)
 
-#d = enchant.Dict("en_US")
-
-with open("./embeddings/glove.6B.50d.txt", "r", encoding='utf8') as f:
+with open("./embeddings/glove.6B.300d.txt", "r", encoding='utf8') as f:
     lines = f.readlines()
 
 count = 0
-with open("./embeddings/glove.6B.50d_small.txt", "w", encoding='utf8') as f:
+with open("./embeddings/glove.6B.300d_small.txt", "w", encoding='utf8') as f:
     for line in lines:
-        if count == 4500:
+        if count == 26423:
             break;
 
         word = line.split()[0]
         contains_num = bool(re.search(r'\d', word))
-        if word == "he" or word == "she":
-            f.write(line)
-            count += 1
-        elif wordnet.synsets(word):
-            if (not contains_num) and (word not in ENGLISH_STOP_WORDS):
+
+        if word in dictionary:
+            if (not contains_num):
                 f.write(line)
                 count += 1
